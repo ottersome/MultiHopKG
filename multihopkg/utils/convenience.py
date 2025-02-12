b8ae206bc32b257eb6169756c56973a138e43f2e
ottersome <luisgarcia26284@gmail.com>
2025-02-11 22:11:13 -0600

    Adding Reward Normalization

--- multihopkg/utils/convenience.py
+++ multihopkg/utils/convenience.py
@@ -1,4 +1,5 @@
 from typing import Type, TypeVar, Any
+import torch
 
 # Define a generic type variable
 T = TypeVar("T")
@@ -20,3 +21,14 @@ def create_placeholder(expected_type: Type[T], name: str, location: str) -> Any:
         )
 
     return placeholder
+
+def tensor_normalization(tensor: torch.Tensor) -> torch.Tensor:
+    """Normalizes a tensor by its mean and standard deviation.
+    Args:
+        tensor: The tensor to normalize.
+    Returns:
+        The normalized tensor.
+    """
+    mean = tensor.mean() + 1e-8
+    std = tensor.std()
+    return (tensor - mean) / std
