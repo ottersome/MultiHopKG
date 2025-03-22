8f426e65f7aea15634ae3de1e052928363beff54
ottersome <luisgarcia26284@gmail.com>
2025-03-22 01:21:11 -0600

    Adding: Some convenience scripts
    - Checking mQuake Dataset

---
+++ scripts/check_mquake.py
@@ -0,0 +1,42 @@
+"""
+This file needs to be run at the repos root
+"""
+import argparse
+import json
+
+def argsies() -> argparse.Namespace:
+    ap = argparse.ArgumentParser()
+    ap.add_argument("--json_file", "-f", default="./temp/MQuAKE-CF.json")
+
+    return ap.parse_args()
+
+def main(args: argparse.Namespace):
+    """
+    Essentially now it just does 
+    """
+    json_file_path: str = args.json_file
+    
+    # Open and load the JSON file
+    with open(json_file_path, 'r') as file:
+        data = json.load(file)
+
+    assert isinstance(data, list), "We expect data to be a list"
+
+    # Data is list
+    print(f"Data is of type {type(data)}")
+    if type(data) is list:
+        print(f"Data is of length {len(data)}")
+    
+    # Show all the keys in the first directory
+    print("Keys in the first directory:")
+    available_keys = set()
+    for elem in data:
+        for key in elem.keys():
+            available_keys.add(key)
+    print("Available keys inside of each element:")
+    print(available_keys)
+
+if __name__ == "__main__":
+    args = argsies()
+    main(args)
+
