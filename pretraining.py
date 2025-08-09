bc611da2e3c0c924dfa8c142d0ba849f6ffa6abe
ottersome <luisgarcia26284@gmail.com>
2025-08-09 00:31:40 -0600

    Misc: Simplifying Path imports a bit

--- pretraining.py
+++ pretraining.py
@@ -445,10 +445,9 @@ def main():
     ########################################
     # Load Embedding Data
     ########################################
-    ent2id, id2ent, rel2id, id2rel = load_dictionaries(args.path_entities_dict, args.path_relations_dict)
-    # TODO: Figure out if we should use the one below
-    # id2ent, ent2id = data_utils.load_inclusive_index(args.path_entities_dict)
-    # id2rel, rel2id = data_utils.load_inclusive_index(args.path_relations_dict)
+    path_entities_dict = os.path.join(args.path_mquake_data, "entities.dict")
+    path_relations_dict = os.path.join(args.path_mquake_data, "relations.dict")
+    ent2id, id2ent, rel2id, id2rel = load_dictionaries(path_entities_dict, path_relations_dict)
     logger.info(f"Loaded a total of :\n\t-{len(id2ent)} entities\n\t-{len(id2rel)} relations")
 
     ########################################
@@ -460,8 +459,9 @@ def main():
         dataset_partitions = load_from_cache(args.path_cache_dir)
     else:
         logger.info("Either cache does not exist or is being force to be recomuted... ")
+        mquake_raw_path = os.path.join(args.path_mquake_data, "MQuAKE-CF.json")
         dataset_partitions = process_qa_dataset(
-            args.path_dataraw,
+            mquake_raw_path,
             args.path_cache_dir,
             args.tvt_split,
             word_tokenizer,
