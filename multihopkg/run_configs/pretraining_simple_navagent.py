b89f6d449d7b500a8af7039ffb2c303a28128c5b
ottersome <luisgarcia26284@gmail.com>
2025-08-21 10:47:29 -0600

    refactor: adjust hyperparameters for navigator pretraining config

--- multihopkg/run_configs/pretraining_simple_navagent.py
+++ multihopkg/run_configs/pretraining_simple_navagent.py
@@ -5,9 +5,9 @@ def arguments() -> argparse.Namespace:
 
     # -- General Training Parameters -- #
     parser.add_argument("--epoch", type=int, default=10)
-    parser.add_argument("--training_batch_size", type=int, default=512)
-    parser.add_argument("--steps_in_episode", type=int, default=8, help="Number of steps in each episode")
-    parser.add_argument("--learning_rate", '-lr', type=float, default=5e-4, help="Learning rate for optimizer")
+    parser.add_argument("--training_batch_size", type=int, default=128)
+    parser.add_argument("--steps_in_episode", type=int, default=4, help="Number of steps in each episode")
+    parser.add_argument("--learning_rate", '-lr', type=float, default=5e-2, help="Learning rate for optimizer")
     parser.add_argument("--checkpoint_dir", type=str, default="./checkpoints/navigator/")
     parser.add_argument("--save_interval", type=int, default=5, help="Save model every N epochs")
     parser.add_argument("--seed", type=int, default=42)
@@ -32,7 +32,7 @@ def arguments() -> argparse.Namespace:
 
     # --- Reinforcement Learning Parameters --- #
     parser.add_argument("--rl_beta", default=0.01, type=float, help="For entropy regularization")
-    parser.add_argument("--rl_gamma", default=0.99, type=float, help="Discount factor for future rewards")
+    parser.add_argument("--rl_gamma", default=0.93, type=float, help="Discount factor for future rewards")
     parser.add_argument("--rl_dim_hidden", default=400, type=int, help="Representation size for policy network")
     parser.add_argument("--env_dim_observation", type=int, default=1000, help="Dimension of observation space (will be overridden)")
     parser.add_argument("--use_navigator", action="store_true", help="Use the navigator model with state-target attention")
